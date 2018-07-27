d1bf84a8eb3016db0247bd503187223c4788b1d3
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-07-27 23:35:22 +0100

    Fri Jul 27 23:35:22 BST 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,6 +4,7 @@
 def checkout(skus):
 
     a_count = 0
+    total_a = 0
     b_count = 0
     price = 0
 
@@ -13,6 +14,7 @@ def checkout(skus):
     products["B"] = 30
     products["C"] = 20
     products["D"] = 15
+    products["E"] = 40
 
     for product in skus:
 
@@ -23,16 +25,23 @@ def checkout(skus):
         if product == "A":
 
             a_count = a_count + 1
+            total_a = total_a + 1
 
         if product == "B":
 
             b_count = b_count + 1
 
-        
+
         price = price + products[product]
 
         if a_count == 3:
             price = price - 20
+            a_count = 0
+
+        if total_a == 5:
+
+            price = price - 30
+            total_a = 0
             a_count = 0
 
         if b_count == 2:
